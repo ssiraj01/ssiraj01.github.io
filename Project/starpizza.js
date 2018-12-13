e760d4fd5994e9cf83dea75fea0d8f1fa8f35a55
ssiraj01 <xsairax@hotmail.com>
2018-12-12 19:17:03 -0800

    Update starpizza.js

--- Project/starpizza.js
+++ Project/starpizza.js
@@ -1,16 +1,7 @@
-// Get the pizza size price and add it to the running Total
-// then pass that running total to the next function so 
-// the next function will add a particular total to the running total and so on...
-//
-// Keep doing this until you get all items added to the running total.
-//
-// Just remember that the syntax will be text1 = text1 + something + "<br>";
-// So you take the orginal value and concetenate to something new and end it with
-// an HTML line break so our code will write the next thing one line below instead
-// of overwriting the previous print out.
+
 function getReceipt() {
-	text1 = ""; // These initialize as empty strings but get passed from  
-	text2 = ""; // function to function, growing line by line into a full receipt
+	text1 = ""; 
+	text2 = ""; 
 	var runningTotal = 0;
 	var sizeTotal = 0;
 	var sizeArray = document.getElementsByClassName("size");
@@ -34,16 +25,10 @@ function getReceipt() {
 		text2 = text2+sizeTotal+"<br>";
 	}
 	runningTotal = sizeTotal;
-	getMeat(runningTotal,text1,text2); // All three of these variables will be passed on to each function
+	getMeat(runningTotal,text1,text2);
 };	
 
-// With both the meat and veggie functions each item in the array will be
-// 1 dollar but the first is going to be free so we can count the total
-// of items in their array and subtract 1 to get the total item cost
-//
-// Now we can add the item cost to our running total to get the new
-// running total and then pass this new running total to the next function
-// Just keep up this process until we've added all items to the running total
+
 function getMeat(runningTotal,text1,text2) {
 	var runningTotal = runningTotal;
 	var meatTotal = 0;
@@ -160,8 +145,7 @@ function getCrust(runningTotal,text1,text2) {
 	document.getElementById("totalPrice2").innerHTML = "</h3>$"+runningTotal+".00"+"</h3>";
 };
 
-// This code clears the form selections to their defaults and then sets the 
-// corresponding div's CSS opacity to 0, effectively hiding it from view.
+
 function clearAll() {
 	document.getElementById("frmMenu").reset();
 	document.getElementById("cart").style.opacity=0;
